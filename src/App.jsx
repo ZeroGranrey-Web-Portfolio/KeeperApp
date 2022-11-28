25c5589a87c09a490471e10cb12288b660a5050e
Yaroslav <yaroslavshlyahovchuk@gmail.com>
2022-11-28 10:05:15 -0600

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Note from "./components/Note";
 import CreateArea from "./components/CreateArea";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const App = () => {
   let i = 0;
+  // localStorage.clear();
   const [notes, setNotes] = useState([]);
 
   const onAddNoteClick = (title, content) => {
@@ -18,6 +19,7 @@ const App = () => {
     setNotes((prevNotes) => {
       return [...prevNotes];
     });
+    localStorage.setItem("notes", JSON.stringify(notes));
   };
 
   const onDelete = (id) => {
@@ -25,6 +27,20 @@ const App = () => {
       return [...prevNotes.filter((v, i) => i !== id)];
     });
   };
+  useEffect(() => {
+    let localNotes = localStorage.getItem("notes");
+    if (localNotes) {
+      localNotes = JSON.parse(localNotes);
+      notes.push(...localNotes);
+    }
+    setNotes((prevNotes) => {
+      return [...prevNotes];
+    });
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
 
   return (
     <>
